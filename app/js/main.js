48e49d3f2d145cde37927c0cabf2dde9fd668c81
By-Biska <varakinkirill3@gmail.com>
2021-08-20 23:11:38 +0300

    questions

--- app/js/main.js
+++ app/js/main.js
@@ -7,4 +7,9 @@ $(function () {
     })
 
 
+    $(".questions__item-title").on('click',function(){
+        $('.questions__item').removeClass('questions__item--active')
+        $(this).parent().addClass('questions__item--active')
+    })
+
 })
