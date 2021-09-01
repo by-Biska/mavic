fba9e757fdf625adaf9602c08c15fdd0049c133c
By-Biska <varakinkirill3@gmail.com>
2021-09-01 14:33:44 +0300

    v1.0

--- gulpfile.js
+++ gulpfile.js
@@ -52,6 +52,7 @@ function scripts() {
     "node_modules/jquery/dist/jquery.js",
     "node_modules/slick-carousel/slick/slick.js",
     "node_modules/fullpage.js/dist/fullpage.js",
+    "node_modules/fullpage.js/vendors/scrolloverflow.js",
     "app/js/main.js"])
     .pipe(concat("main.min.js"))
     .pipe(uglify())
